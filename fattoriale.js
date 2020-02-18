2be0492ab890682d3877bcd0d72d68afc4de5201
kenzo78 <chrispal@hotmail.it>
2020-02-18 21:45:52 +0100

    Update fattoriale.js

--- fattoriale.js
+++ fattoriale.js
@@ -3,7 +3,7 @@
 **/
 function fatto(n) {
     if (n < 0) {
-        return -1;
+       return "Per definizione il fattoriale si calcola solo sui numeri Naturali (Interi positivi) ";
     }
     else if (n === 0) {
         return 1;
